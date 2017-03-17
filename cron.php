f62bc97d29333beeac4ab7a1e40b6a78d5735eb5
Mark Wienk <mark@wienkit.nl>
2017-03-17 11:40:34 +0100

    [BUGFIX] Multistore must use multiple cron commands

--- cron.php
+++ cron.php
@@ -21,10 +21,6 @@ require_once(dirname(__FILE__).'/controllers/admin/AdminBolPlazaOrdersController
 if (Tools::getIsset('secure_key')) {
     $secureKey = md5(_COOKIE_KEY_.Configuration::get('PS_SHOP_NAME').'BOLPLAZA');
     if (!empty($secureKey) && $secureKey === $_GET['secure_key']) {
-        $shop_ids = Shop::getCompleteListOfShopsID();
-        foreach ($shop_ids as $shop_id) {
-            Shop::setContext(Shop::CONTEXT_SHOP, (int)$shop_id);
-            BolPlaza::synchronize();
-        }
+        BolPlaza::synchronize();
     }
 }
