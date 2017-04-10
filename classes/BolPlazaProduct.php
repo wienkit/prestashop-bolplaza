58adee63f66c8d459a2900bdd8b9181e2c3ae7c6
Mark Wienk <mark@wienkit.nl>
2017-04-10 17:34:50 +0200

    [Bugfix] Use ID instead of id_bolproduct

--- classes/BolPlazaProduct.php
+++ classes/BolPlazaProduct.php
@@ -151,7 +151,7 @@ class BolPlazaProduct extends ObjectModel
         }
         $offer->QuantityInStock = $stock;
         $offer->Publish = $this->published == 1 ? 'true' : 'false';
-        $offer->ReferenceCode = $this->id_bolplaza_product;
+        $offer->ReferenceCode = $this->id;
         return $offer;
     }
 
