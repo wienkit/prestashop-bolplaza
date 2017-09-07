b36aec9b4f4f2ac9bf093f9ef69210ee09cfc171
Mark Wienk <mark@wienkit.nl>
2017-09-07 17:14:53 +0200

    Added dummy test

--- tests/acceptance/SetupModuleTest.php
+++ tests/acceptance/SetupModuleTest.php
@@ -6,10 +6,15 @@ class SetupModuleTest extends \PHPUnit_Extensions_Selenium2TestCase
 {
     public function setUp()
     {
-        $this->setHost('selenium');
+        $this->setHost('selenium-standalone-firefox');
         $this->setPort(4444);
         $this->setBrowserUrl('http://localhost');
-        $this->setBrowser('chrome');
+        $this->setBrowser('firefox');
+    }
+
+    public function testTheThing()
+    {
+        $this->assertTrue(true);
     }
 
     public function tearDown()
---
+++ tests/autoload.php
@@ -0,0 +1,2 @@
+<?php
+include_once dirname(__FILE__).'/../libraries/autoload.php';
